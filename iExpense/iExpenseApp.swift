bc81103caa7724f94c6bbf007afc42659304cd4b
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-01-25 10:44:04 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Francisco Manuel Gallegos Luque on 25/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iExpenseTests/iExpenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseTests.swift
+//  iExpenseTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 25/01/2025.
+//
+
+import Testing
+@testable import iExpense
+
+struct iExpenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
